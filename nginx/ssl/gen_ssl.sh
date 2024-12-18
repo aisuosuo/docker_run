73bc64e823553756c63756e8bc82d68e5369b1db
Feng Wang <879937365@qq.com>
2024-12-18 13:00:15 +0800

    Update gen_ssl.sh

--- nginx/ssl/gen_ssl.sh
+++ nginx/ssl/gen_ssl.sh
@@ -3,10 +3,8 @@
 openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout harbridgecrypto.com.key -out harbridgecrypto.com.crt
 
 
-
-openssl genrsa -out harbridgecrypto.key 2048
-
-openssl req -new -key harbridgecrypto.key -out harbridgecrypto.csr
+openssl req -new -key bridgesecsys.key -out bridgesecsys.csr
+openssl x509 -req -days 365 -in bridgesecsys.csr -signkey bridgesecsys.key -out bridgesecsys.crt
 
 AU
 NSW
